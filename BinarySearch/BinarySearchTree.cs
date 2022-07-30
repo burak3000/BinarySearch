4219b2ad6385c0cf385c2107378fdfd50960e269
Burak Ucbinli <burakucbinli@gmail.com>
2022-07-30 18:45:21 +0300

    Add locking for multithreading

--- BinarySearch/BinarySearchTree.cs
+++ BinarySearch/BinarySearchTree.cs
@@ -1,145 +1,261 @@
 ﻿using System;
-using System.Linq;
 
 namespace BinarySearch
 {
     public class BinarySearchTree
     {
+        private static object s_Lock = new object();
         public CommandNode Root { get; set; }
-        public int Count { get; set; }
-
+        private int m_Count = 0;
+        public int Count => m_Count;
+        public CommandNode CommandWithMaxPrio { get; }
+        /// <summary>
+        /// Adds new entry to the tree. If a node with same priority exists in the tree, its commands will be appended to the existing ones command list.
+        /// Because of this, when we use find method on this entry, already added object will be returned.
+        /// For example we add CommandNode with prio 1. Then we add new CommandNode with prio 1. Commands of latter one will be appended to the first one.
+        /// When we call find CommandNode with prio 1, only the address of the first one is valid.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns>true if added, otherwise false</returns>
         public bool Add(CommandNode node)
         {
-            CommandNode currentNode = this.Root;
-            CommandNode lastVisitedNode = null;
-            try
+            lock (s_Lock)
             {
-                while (currentNode != null)
+                if (node == null)
                 {
-                    lastVisitedNode = currentNode;
-                    if (node.Priority < currentNode.Priority) //Is new node in left tree? 
-                        currentNode = currentNode.LeftNode;
-                    else if (node.Priority > currentNode.Priority) //Is new node in right tree?
-                        currentNode = currentNode.RightNode;
+                    return false;
+                }
+
+                CommandNode currentNode = this.Root;
+                CommandNode lastVisitedNode = null;
+                try
+                {
+                    while (currentNode != null)
+                    {
+                        lastVisitedNode = currentNode;
+                        if (node.Priority < currentNode.Priority) //Is new node in left tree? 
+                            currentNode = currentNode.LeftNode;
+                        else if (node.Priority > currentNode.Priority) //Is new node in right tree?
+                            currentNode = currentNode.RightNode;
+                        else
+                        {
+                            //Same priority exists, add new commands to this priorities list.
+                            if (node.Commands != null && node.Commands.Count > 0)
+                            {
+                                currentNode.Commands.AddRange(node.Commands);
+                                return true;
+                            }
+                            return false;
+                        }
+                    }
+
+                    if (Root == null)//Tree ise empty
+                    {
+                        Root = node;
+                    }
                     else
                     {
-                        //Same priority exists, add new commands to this priorities list.
-                        if (node.Commands != null && node.Commands.Count > 0)
+                        if (node.Priority < lastVisitedNode.Priority)
                         {
-                            currentNode.Commands.AddRange(node.Commands);
-                            return true;
+                            lastVisitedNode.LeftNode = node;
                         }
-                        return false;
+                        else
+                        {
+                            lastVisitedNode.RightNode = node;
+                        }
                     }
+                    m_Count++;
+                    return true;
                 }
-                CommandNode newNode = new CommandNode();
-                newNode.Priority = node.Priority;
-                newNode.Commands = node.Commands;
-
-                if (this.Root == null)//Tree ise empty
-                    this.Root = newNode;
-                else
+                catch (Exception e)
                 {
-                    if (node.Priority < lastVisitedNode.Priority)
-                        lastVisitedNode.LeftNode = newNode;
-                    else
-                        lastVisitedNode.RightNode = newNode;
+                    return false;
                 }
-                Count++;
-                return true;
             }
-            catch (Exception e)
-            {
-                return false;
-            }
-
         }
 
         public CommandNode Find(int priority)
         {
-            return this.Find(priority, this.Root);
+            lock (s_Lock)
+            {
+                return Find(priority, this.Root);
+            }
         }
 
-        public void Remove(CommandNode node)
+        public CommandNode Find(CommandNode nodeToFind)
         {
-            this.Root = Remove(this.Root, node);
-            Count--;
+            return Find(nodeToFind, this.Root);
         }
-        private CommandNode Remove(CommandNode parent, CommandNode node)
+
+        public bool Remove(CommandNode node)
         {
-            if (parent == null)
+            try
             {
-                return parent;
+                lock (s_Lock)
+                {
+                    Root = Remove(this.Root, node);
+                    if (m_Count > 0)
+                    {
+                        m_Count--;
+                    }
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private CommandNode Remove(CommandNode currentNode, CommandNode node)
+        {
+            if (currentNode == null || node == null)
+            {
+                return currentNode;
             }
 
-            if (node.Priority < parent.Priority)
+            if (node.Priority < currentNode.Priority)
             {
-                parent.LeftNode = Remove(parent.LeftNode, node);
+                //There is no less entry. We must stop searching
+                if (currentNode.LeftNode == null)
+                {
+                    throw new ArgumentException("Node to be removed could not be found.");
+                }
+                currentNode.LeftNode = Remove(currentNode.LeftNode, node);
             }
-            else if (node.Priority > parent.Priority)
+            else if (node.Priority > currentNode.Priority)
             {
-                parent.RightNode = Remove(parent.RightNode, node);
+                //There is no bigger entry. We must stop searching
+                if (currentNode.RightNode == null)
+                {
+                    throw new ArgumentException("Node to be removed could not be found.");
+                }
+                currentNode.RightNode = Remove(currentNode.RightNode, node);
             }
 
-            // if value is same as parent's value, then this is the node to be deleted  
+            // if value is same as currentNode's value, then this is the node to be deleted  
             else
             {
                 // node with only one child or no child  
-                if (parent.LeftNode == null)
+                if (currentNode.LeftNode == null)
                 {
-                    return parent.RightNode;
+                    return currentNode.RightNode;
                 }
-                else if (parent.RightNode == null)
+                else if (currentNode.RightNode == null)
                 {
-                    return parent.LeftNode;
+                    return currentNode.LeftNode;
                 }
 
                 // node with two children: Get the inorder successor (smallest in the right subtree)  
-                parent.Priority = MinValue(parent.RightNode);
+                currentNode.Priority = MinValue(currentNode.RightNode);
 
                 // Delete the inorder successor  
-                parent.RightNode = Remove(parent.RightNode, parent);
+                currentNode.RightNode = Remove(currentNode.RightNode, currentNode);
             }
 
-            return parent;
+            return currentNode;
         }
         private int MinValue(CommandNode node)
         {
-            int minv = node.Priority;
-
-            while (node.LeftNode != null)
+            lock (s_Lock)
             {
-                minv = node.LeftNode.Priority;
-                node = node.LeftNode;
-            }
+                int minv = node.Priority;
 
-            return minv;
-        }
-        private CommandNode Find(int value, CommandNode parent)
-        {
-            if (parent != null)
-            {
-                if (value == parent.Priority) return parent;
-                if (value < parent.Priority)
-                    return Find(value, parent.LeftNode);
-                else
-                    return Find(value, parent.RightNode);
-            }
-
-            return null;
-        }
-
-        public CommandNode FindNextPrioritizedCommand(CommandNode parent)
-        {
-            if (parent != null)
-            {
-                if (parent.RightNode == null)
+                while (node.LeftNode != null)
                 {
-                    return parent;
+                    minv = node.LeftNode.Priority;
+                    node = node.LeftNode;
                 }
-                parent = FindNextPrioritizedCommand(parent.RightNode);
+
+                return minv;
             }
-            return parent;
+
+        }
+        private CommandNode Find(int priority, CommandNode currentNode)
+        {
+            lock (s_Lock)
+            {
+                if (currentNode != null)
+                {
+                    if (priority == currentNode.Priority)
+                    {
+                        return currentNode;
+                    }
+
+                    if (priority < currentNode.Priority)
+                    {
+                        if (currentNode.LeftNode == null)
+                        {
+                            return null;
+                        }
+                        return Find(priority, currentNode.LeftNode);
+                    }
+                    else
+                    {
+                        if (currentNode.RightNode == null)
+                        {
+                            return null;
+                        }
+                        return Find(priority, currentNode.RightNode);
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        private CommandNode Find(CommandNode nodeToFind, CommandNode currentNode)
+        {
+            lock (s_Lock)
+            {
+                if (currentNode != null)
+                {
+                    if (nodeToFind == currentNode)
+                    {
+                        return currentNode;
+                    }
+
+                    if (nodeToFind.Priority < currentNode.Priority)
+                    {
+                        if (currentNode.LeftNode == null)
+                        {
+                            return null;
+                        }
+                        return Find(nodeToFind, currentNode.LeftNode);
+                    }
+                    else if (nodeToFind.Priority > currentNode.Priority)
+                    {
+                        if (currentNode.RightNode == null)
+                        {
+                            return null;
+                        }
+                        return Find(nodeToFind, currentNode.RightNode);
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        public CommandNode FindNextPrioritizedCommand()
+        {
+            lock (s_Lock)
+            {
+                return FindNextPrioritizedCommand(Root);
+            }
+        }
+
+        private CommandNode FindNextPrioritizedCommand(CommandNode currentNode)
+        {
+            if (currentNode != null)
+            {
+                if (currentNode.RightNode == null)
+                {
+                    return currentNode;
+                }
+                currentNode = FindNextPrioritizedCommand(currentNode.RightNode);
+            }
+
+            return currentNode;
         }
     }
 }
--- BinarySearch/UnitTest1.cs
+++ BinarySearch/UnitTest1.cs
@@ -7,11 +7,16 @@ namespace BinarySearch
     public class UnitTest1
     {
         BinarySearchTree m_Tree;
+        #region SetUp
+
         [SetUp]
         public void InitializeTree()
         {
             m_Tree = new BinarySearchTree();
         }
+        #endregion
+
+        #region Add Tests
 
         [Test]
         public void Add_NullEntry_ReturnsFalse()
@@ -22,7 +27,6 @@ namespace BinarySearch
         [Test]
         public void Add_NewEntry_ElementCountIs1()
         {
-            m_Tree = new BinarySearchTree();
             Assert.True(m_Tree.Add(new CommandNode { Priority = 3 }));
             Assert.AreEqual(1, m_Tree.Count);
         }
@@ -45,15 +49,157 @@ namespace BinarySearch
         }
 
         [Test]
-        public void Find_ExistingPriority_ReturnsCommandNode()
+        public void Add_Add_2EntriesWithSamePriority_ElementCountIs1AndCommandListWillBeAppended()
+        {
+            int node1CommandCount = 3;
+            var node1 = new CommandNode { Priority = 1 };
+            for (int i = 0; i < node1CommandCount; i++)
+            {
+                node1.Commands.Add(new CommandImpl());
+            }
+
+            int node2CommandCount = 5;
+            var node2 = new CommandNode { Priority = 1 };
+            for (int i = 0; i < node2CommandCount; i++)
+            {
+                node2.Commands.Add(new CommandImpl());
+            }
+
+            m_Tree.Add(node1);
+            m_Tree.Add(node2);
+            Assert.AreEqual(8, m_Tree.Find(1).Commands.Count);
+        }
+
+        #endregion
+
+        [Test]
+        public void Find_WithPriority_ExistingPriority_ReturnsCommandNode()
         {
             int priority = 1;
             var node = new CommandNode { Priority = priority };
             m_Tree.Add(node);
-            Assert.AreEqual(node, m_Tree.Find(priority));
+            var foundItem = m_Tree.Find(priority);
+            Assert.AreEqual(node, foundItem);
+        }
 
+        [Test]
+        public void Find_WithCommandNodeObject_ExistingPriority_ReturnsCommandNode()
+        {
+            int priority = 1;
+            var node = new CommandNode { Priority = priority };
+            m_Tree.Add(node);
+            var foundItem = m_Tree.Find(node);
+            Assert.AreEqual(node, foundItem);
+        }
+
+        [Test]
+        public void Find_WithCommandNodeObject_NonExistingSearhItem_ReturnsNull()
+        {
+            int priority = 1;
+            var node = new CommandNode { Priority = priority };
+
+            var foundItem = m_Tree.Find(node);
+            Assert.Null(foundItem);
+        }
+
+        [Test]
+        public void Find_WithPriority_NonExistingSearhItem_ReturnsNull()
+        {
+            int priority = 1;
+            var node = new CommandNode { Priority = priority };
+
+            var foundItem = m_Tree.Find(++priority);
+            Assert.Null(foundItem);
+        }
+
+        [Test]
+        public void Find_NonExistingSearhItem_ReturnsNull()
+        {
+            int priority = 1;
+            var node = new CommandNode { Priority = priority };
+
+            var foundItem = m_Tree.Find(node);
+            Assert.Null(foundItem);
+        }
+
+        [Test]
+        public void Find_TwoNewlyAddedSamePrioCommandNodes_ReturnsFirstAddedCommandNode()
+        {
+            int priority = 1;
+            var node = new CommandNode { Priority = priority };
+            var node2 = new CommandNode { Priority = priority };
+            m_Tree.Add(node);
+            m_Tree.Add(node2);
+            var foundItem = m_Tree.Find(priority);
+            Assert.AreEqual(node, foundItem);
+            /*If CommandNodes with same priority is added,
+            command list of latter node will be appended to
+            the command list of previously added one.*/
+            Assert.AreNotEqual(node2, foundItem);
+        }
+
+        [Test]
+        public void Remove_WithCommandNode_NonExistingCommandNode_ReturnsFalse()
+        {
+            int elementCount = 10;
+            CommandNode node = new CommandNode { Priority = elementCount / 2 };
+            //Add one element to the root
+            m_Tree.Add(node);
+            for (int i = 0; i < elementCount / 2; i++)
+            {
+                //Add two elements for each iteration to the right and left side of the root.
+                CommandNode node2 = new CommandNode { Priority = i };
+                CommandNode node3 = new CommandNode { Priority = elementCount - i };
+                m_Tree.Add(node2);
+                m_Tree.Add(node3);
+            }
+            m_Tree.Remove(node);
+            m_Tree.Remove(m_Tree.Find(3));
+
+            //Root element is added and then for loop two elements in each iteration. We removed 2 of them. 
+            Assert.AreEqual(9, m_Tree.Count);
         }
 
 
+        [Test]
+        public void Remove_NonExistingElement_CannotRemoveAnything()
+        {
+            int elementCount = 10;
+            CommandNode node = new CommandNode { Priority = elementCount / 2 };
+            //Add one element to the root
+            m_Tree.Add(node);
+            for (int i = 0; i < elementCount / 2; i++)
+            {
+                //Add two elements for each iteration to the right and left side of the root.
+                CommandNode node2 = new CommandNode { Priority = i };
+                CommandNode node3 = new CommandNode { Priority = elementCount - i };
+                m_Tree.Add(node2);
+                m_Tree.Add(node3);
+            }
+            m_Tree.Remove(node);
+            m_Tree.Remove(m_Tree.Find(3));
+
+            //Root element is added and then for loop two elements in each iteration. We removed 2 of them. 
+            Assert.AreEqual(9, m_Tree.Count);
+
+            CommandNode nonExistingNode = new CommandNode { Priority = ++elementCount };
+            m_Tree.Remove(nonExistingNode);
+            Assert.AreEqual(9, m_Tree.Count);
+        }
+
+        [Test]
+        public void FindNextPrioritizedCommand()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                m_Tree.Add(new CommandNode { Priority = i });
+                Assert.AreEqual(i, m_Tree.FindNextPrioritizedCommand().Priority);
+            }
+            for (int i = 9; i >= 1; i--)
+            {
+                m_Tree.Remove(m_Tree.Find(i));
+                Assert.AreEqual(i - 1, m_Tree.FindNextPrioritizedCommand().Priority);
+            }
+        }
     }
 }
